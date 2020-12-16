2b6e76f237c0fdf8e3b25d35537c81ae479d1327
sharonwang1220 <71386393+sharonwang1220@users.noreply.github.com>
2020-12-16 21:18:06 +0800

    更新commit部分功能

--- src/object/Blob.java
+++ src/object/Blob.java
@@ -1,4 +1,63 @@
 package object;
 
-public class Blob {
+import functions.Hash;
+import functions.KeyValueStore;
+
+import java.io.File;
+import java.io.IOException;
+import java.security.MessageDigest;
+
+/*
+ * 待完成的任务：
+ * 1. 实现toString方法
+ * */
+public class Blob extends KVObject {
+
+    private final String filepath;
+
+    // 根据文件路径创建blob的构造方法
+    public Blob(String path) {
+        objectType = "blob";
+        filepath = path;
+        try {
+            key = Hash.stringHash(KeyValueStore.readFileContent(path));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+        filename = filepath.substring(filepath.lastIndexOf(File.separator));
+    }
+
+    // 根据File对象创建blob的方法
+    public Blob(File file) {
+        objectType = "blob";
+        filepath = file.getAbsolutePath();
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-1");
+            Hash.fileHash(file, md);
+            key = Hash.digest(md);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+        filename = file.getName();
+    }
+
+    public String getFilepath() {
+        return filepath;
+    }
+
+    @Override
+    public void store() {
+        try {
+            KeyValueStore.blobStore(this);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String toString () {
+        return objectType;
+    }
 }
--- src/object/Commit.java
+++ src/object/Commit.java
@@ -1,4 +1,34 @@
 package object;
 
-public class Commit {
+import functions.Hash;
+import java.security.MessageDigest;
+
+public class Commit extends KVObject {
+    private final Commit lastcommit;
+    private final Tree TreeRoot;
+
+    //根据tree和lastcommit创建Commit对象
+    public Commit(Tree TreeRoot, Commit lastcommit) {
+        objectType = "commit";
+        this.TreeRoot = TreeRoot;
+        this.lastcommit = lastcommit;
+
+        // 计算commit对象的hash值
+        MessageDigest md;
+
+        try {
+            md = MessageDigest.getInstance("SHA-1");
+            Hash.commitHash(TreeRoot, lastcommit, md);
+            key = Hash.digest(md);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    @Override
+    public void store() {
+
+    }
 }
